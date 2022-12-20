ec76bf02b0963bca634ca4064b086da02a8a1d6c
Aaron Hsu <aaron.hsu@duke.edu>
2022-12-20 16:54:29 -0500

    moved into correct folder

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221220.165233';
+const cacheName = 'chirpy-20221220.165421';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
