1625ca24c5b11a3c5d7162fae7f7870c90539b55
Aaron Hsu <aaron.hsu@duke.edu>
2022-12-20 16:53:32 -0500

    workflow gh pagse

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221220.164243';
+const cacheName = 'chirpy-20221220.165233';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
