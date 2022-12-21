3edd5c3fbac8864199e17886ff0a8e87f2bfcd1f
Aaron Hsu <aaron.hsu@duke.edu>
2022-12-21 03:26:19 -0500

    readme

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221221.075641';
+const cacheName = 'chirpy-20221221.082248';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
